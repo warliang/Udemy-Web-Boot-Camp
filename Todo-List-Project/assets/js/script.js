f8a4b770ea7b2f9eeb260243f624431f20d5caa5
unknown <war.liang96@gmail.com>
2019-01-16 23:08:53 -0800

    completed todo assignment

---
+++ Todo-List-Project/assets/js/script.js
@@ -0,0 +1,24 @@
+// Check off specific todos by clicking
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+//Click on delete button to delete todo
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type=text]").keypress(function(event) {
+	if (event.which === 13) {
+		var newToDo = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + newToDo + "</li>");
+	}
+});
+
+$(".fa-plus").click(function() {
+	$("input[type=text]").fadeToggle();
+});
